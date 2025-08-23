1c3fdb2c2ead477143451fa95433f608b8ef2144
pingpongpande <adityapande0075@gmail.com>
2025-08-23 10:58:55 +0530

    Updated Optiverse folders

--- main_copy_from_optiverse.py
+++ main_copy_from_optiverse.py
@@ -5,17 +5,17 @@ def main():
     this_dir = os.path.abspath(os.path.dirname(__file__))
 
     # 1. Folders that will be copied as is from optiverse with the 'optiverse_' prefix
-    folders_to_copy = [
+    folders_to_copy_from_optiverse = [
         os.path.join(optiverse_dir, 'connectors'),
         os.path.join(optiverse_dir, 'utils'),
     ]
-    for folder in folders_to_copy:
+    for folder in folders_to_copy_from_optiverse:
         dest_folder = os.path.join(this_dir, 'optiverse_' + os.path.basename(folder))
         os.system(f"cp -r {folder} {dest_folder}")  # Copy and even rewrite if exists
 
     # Git commit and push changes
     os.system("git add .")
-    os.system("git commit -m 'Update Optiverse copies'")
+    os.system("git commit -m 'Updated Optiverse folders'")
     os.system("git push")
 
 
