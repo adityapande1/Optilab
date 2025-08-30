e9a4de3d6ea748c222317321c89054d7d25a1846
pingpongpande <adityapande0075@gmail.com>
2025-08-30 09:13:29 +0530

    Updated Constants.py

--- Constants.py
+++ Constants.py
@@ -1,3 +1,19 @@
-GLOBAL_DB_FOLDERPATH = './database'
-NIFTY_PARQUET_PATH = './database/indices/NIFTY_50_1min.parquet'
-NIFTY_EXPIRIES_JSON_PATH = './datamanager/metadata/nifty_expiries.json'
+from pathlib import Path
+
+# Project root is the folder containing this constants.py
+PROJECT_ROOT = Path(__file__).resolve().parent
+
+# Common Paths
+GLOBAL_DB_FOLDERPATH = PROJECT_ROOT / "database"
+EQUITY_FOLDERPATH = GLOBAL_DB_FOLDERPATH / "equity"
+INDICES_FOLDERPATH = GLOBAL_DB_FOLDERPATH / "indices"
+OPTIONS_FOLDERPATH = GLOBAL_DB_FOLDERPATH / "options"
+
+BACKTEST_RESULTS_FOLDERPATH = PROJECT_ROOT / "backtest_results"
+
+# Nifty Specific Paths
+NIFTY_PARQUET_PATH = GLOBAL_DB_FOLDERPATH / "indices" / "NIFTY_50_1min.parquet"
+NIFTY_EXPIRIES_JSON_PATH = PROJECT_ROOT / "datamanager" / "metadata" / "nse" / "nifty_expiries.json"
+NSE_HOLIDAYS_JSON_PATH = PROJECT_ROOT / "datamanager" / "metadata" / "nse" / "nse_holidays.json"
+
+
